e145b6cd12a49a9a2754ec3aa18294f9a8d29a51
Fabio Mattei <burattino@gmail.com>
2015-08-08 17:35:25 +0200

    working on the post type

--- include/register-posttype.php
+++ include/register-posttype.php
@@ -1,19 +1,19 @@
 <?php
 	
 $labels = array(
-	'name' => _x( 'Ultimate Responsive Image Slider', RCSL_TEXT_DOMAIN ),
-	'singular_name' => _x( 'Ultimate Responsive Image Slider', RCSL_TEXT_DOMAIN ),
-	'add_new' => _x( 'Add New Image Slider', RCSL_TEXT_DOMAIN ),
-	'add_new_item' => _x( 'Add New Image Slider', RCSL_TEXT_DOMAIN ),
-	'edit_item' => _x( 'Edit Image Slider', RCSL_TEXT_DOMAIN ),
-	'new_item' => _x( 'New Image Slider', RCSL_TEXT_DOMAIN ),
-	'view_item' => _x( 'View Image Slider', RCSL_TEXT_DOMAIN ),
-	'search_items' => _x( 'Search Image Slider', RCSL_TEXT_DOMAIN ),
-	'not_found' => _x( 'No Image Slider found', RCSL_TEXT_DOMAIN ),
+	'name' => _x( 'Cherry Slider', RCSL_TEXT_DOMAIN ),
+	'singular_name' => _x( 'Cherry Slider', RCSL_TEXT_DOMAIN ),
+	'add_new' => _x( 'Add New Cherry Slider', RCSL_TEXT_DOMAIN ),
+	'add_new_item' => _x( 'Add New Cherry Slider', RCSL_TEXT_DOMAIN ),
+	'edit_item' => _x( 'Edit Cherry Slider', RCSL_TEXT_DOMAIN ),
+	'new_item' => _x( 'New Cherry Slider', RCSL_TEXT_DOMAIN ),
+	'view_item' => _x( 'View Cherry Slider', RCSL_TEXT_DOMAIN ),
+	'search_items' => _x( 'Search Cherry Slider', RCSL_TEXT_DOMAIN ),
+	'not_found' => _x( 'No Cherry Slider found', RCSL_TEXT_DOMAIN ),
 	'not_found_in_trash' => _x( 'No Image found in Trash', RCSL_TEXT_DOMAIN ),
 	'parent_item_colon' => _x( 'Parent Image:', RCSL_TEXT_DOMAIN ),
-	'all_items' => __( 'All Image Sliders', RCSL_TEXT_DOMAIN ),
-	'menu_name' => _x( 'UR Image Slider', RCSL_TEXT_DOMAIN ),
+	'all_items' => __( 'All Cherry Sliders', RCSL_TEXT_DOMAIN ),
+	'menu_name' => _x( 'Cherry Slider', RCSL_TEXT_DOMAIN ),
 );
 
 $args = array(
@@ -36,3 +36,34 @@ $args = array(
 );
 
 register_post_type( RCSL_TEXT_DOMAIN, $args );
+
+/**
+ * Managin columns for post type list 
+ */
+function cherry_slider_columns( $columns ){
+    $columns = array(
+        'cb' => '<input type="checkbox" />',
+        'title' => __( 'Cherry Slider Title', RCSL_TEXT_DOMAIN ),
+        'shortcode' => __( 'Slider Shortcode', RCSL_TEXT_DOMAIN ),
+        'date' => __( 'Date', RCSL_TEXT_DOMAIN )
+    );
+    return $columns;
+}
+
+add_filter( 'manage_edit-cherry-slider_columns', 'cherry_slider_columns' );
+
+/**
+ * Populating columns with the right content
+ */
+function ris_gallery_manage_columns( $column, $post_id ){
+    global $post;
+    switch( $column ) {
+      case 'shortcode' :
+        echo '<input type="text" value="[RCSL id='.$post_id.']" readonly="readonly" />';
+        break;
+      default :
+        break;
+    }
+}
+
+add_action( 'manage_cherry-slider_posts_custom_column', 'ris_gallery_manage_columns' , 10, 2 );
